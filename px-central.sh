27919c77c6f807c064aa9a8ec4ef59927851a3c7
Francois Martel <francoismartel@Francoiss-iPro.fios-router.home>
2019-01-27 17:55:30 -0500

    vagrant specifics

--- px-central.sh
+++ px-central.sh
@@ -10,17 +10,17 @@ curl -o px-prometheus.yaml "https://docs.portworx.com/samples/k8s/prometheus/07-
 mkdir /tmp/grafanaConfigurations
 curl -o /tmp/grafanaConfigurations/Portworx_Volume_template.json -s https://raw.githubusercontent.com/portworx/px-docs/gh-pages/k8s-samples/grafana/dashboards/Portworx_Volume_template.json
 curl -o /tmp/grafanaConfigurations/dashboardConfig.yaml -s https://raw.githubusercontent.com/portworx/px-docs/gh-pages/k8s-samples/grafana/config/dashboardConfig.yaml
-# kubectl create configmap grafana-config --from-file=/tmp/grafanaConfigurations -n kube-system
+kubectl create configmap grafana-config --from-file=/tmp/grafanaConfigurations -n kube-system
 curl -s https://docs.portworx.com/samples/k8s/grafana/grafana-deployment.yaml | sed 's/config.yaml/dashboardConfig.yaml/g;/- port: 3000/a\\    nodePort: 30950' > grafana-deployment.yaml
 sudo curl -s http://openstorage-stork.s3-website-us-east-1.amazonaws.com/storkctl/2.0.0/linux/storkctl -o /usr/bin/storkctl
 sudo chmod +x /usr/bin/storkctl
 if [ $(hostname) != master-1 ]; then
   while : ; do
-    token=$(ssh -oConnectTimeout=1 -oStrictHostKeyChecking=no node-#{c}-1 pxctl cluster token show | cut -f 3 -d " ")
+    token=$(ssh -oConnectTimeout=1 -oStrictHostKeyChecking=no vagrant@k1n1 pxctl cluster token show | cut -f 3 -d " ")
     echo $token | grep -Eq '.{128}'
     [ $? -eq 0 ] && break
     sleep 5
   done
-  storkctl generate clusterpair -n default remotecluster-#{c} | sed '/insert_storage_options_here/c\\    ip: node-#{c}-1\\n    token: '$token >/root/cp.yaml
-  cat /root/cp.yaml | ssh -oConnectTimeout=1 -oStrictHostKeyChecking=no master-1 kubectl apply -f -
+  storkctl generate clusterpair -n default remotecluster-1 | sed '/insert_storage_options_here/c\\    ip: k1n1\n    token: '$token >cp.yaml
+  cat cp.yaml | ssh -oConnectTimeout=1 -oStrictHostKeyChecking=no vagrant@k1m kubectl apply -f -
 fi
