9547b9c8c5d7972594619f7f3364049cd274de6c
Francois Martel <francoismartel@Francoiss-iPro.fios-router.home>
2019-02-22 08:44:36 -0500

    fixed minio-svc

--- create-ingress.sh
+++ create-ingress.sh
@@ -1,5 +1,4 @@
   kubectl create -f rbac.yaml
-  kubectl create -f default-backend.yaml
   kubectl create ns ingress-nginx
   kubectl create -f default-backend.yaml
   kubectl create -f nginx-controller.yaml
